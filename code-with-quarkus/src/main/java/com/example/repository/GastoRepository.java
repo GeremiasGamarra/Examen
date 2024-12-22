125a884b70cf85b22063459350e59a54ae8b022f
GeremiasGamarra <gemiasgamarra204@gmail.com>
2024-12-22 20:09:52 -0300

    Mas

--- code-with-quarkus/src/main/java/com/example/repository/GastoRepository.java
+++ code-with-quarkus/src/main/java/com/example/repository/GastoRepository.java
@@ -1,5 +1,9 @@
 package com.example.repository;
 
+import com.example.model.Gasto;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
